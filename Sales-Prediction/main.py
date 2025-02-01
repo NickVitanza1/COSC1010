e4398bdf8053bf45f582a0369ad80788dc581e00
NickVitanza1 <nickvitanza@westernwyoming.edu>
2025-02-01 02:31:07 +0000

    ready for grading

--- Sales-Prediction/main.py
+++ Sales-Prediction/main.py
@@ -8,7 +8,8 @@
 # Variables to hold the sales total and the profit
 
 # Get the amount of projected sales.
-
+total_sales = float(input('Enter the projected sales: '))
 # Calculate the projected profit.
-
+profit = total_sales * 0.23
 # Print the projected profit.
+print('The profit is $', profit)
