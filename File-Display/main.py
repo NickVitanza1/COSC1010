6868af4b8abfcc664d4252a4afb9df97259a0d07
NickVitanza1 <nickvitanza@westernwyoming.edu>
2025-03-15 00:13:18 +0000

    ready for grading

--- File-Display/main.py
+++ File-Display/main.py
@@ -1,7 +1,18 @@
 #
-# Name
-# Date
+# Nicholas Vitanza
+# 3/14/25
 # File Display Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+
+# Open the file
+myfile = open('numbers.txt', 'r')
+
+# Read and diplay the files contents
+for line in myfile:
+    number = int(line)
+    print(number)
+
+# Close the file
+myfile.close()
